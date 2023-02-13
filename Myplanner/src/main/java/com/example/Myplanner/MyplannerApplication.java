414643f578881f4a6e993fb17964145f13bb8d70
morar0108 <maria.morar2001@yahoo.com>
2023-02-13 22:41:49 +0200

    Add repo to git

---
+++ Myplanner/src/main/java/com/example/Myplanner/MyplannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Myplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyplannerApplication.class, args);
+	}
+
+}
---
+++ Myplanner/src/test/java/com/example/Myplanner/MyplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Myplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
